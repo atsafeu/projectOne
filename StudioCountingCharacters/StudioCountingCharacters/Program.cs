18b2a5e948ebd56482675dd77f47bfa71c9e95e5
Armand Tsafack <armandtsafack@armandtsafack-2.home>
2020-10-03 17:49:09 -0500

    add studio

---
+++ StudioCountingCharacters/StudioCountingCharacters/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StudioCountingCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+            string test = "Hello World";
+            Dictionary<char,int>counts = new Dictionary<char,int>();
+            foreach (char c in test)
+            {
+               if (!counts.Keys.Contains(c))
+                {
+                    counts.Add(c,1);
+                 }
+               else
+                {
+                    counts[c]++;
+                }
+               foreach(KeyValuePair<chair,int> count in counts)
+                {
+                    Console.WriteLine(count.Key + ":" + count.Value);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
